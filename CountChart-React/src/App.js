dbe47e79678e5b4860b5e871b66a8c493f33b8a3
HareemA <ahareem134@gmail.com>
2023-09-21 12:13:43 -0700

    Frame displayed and slider added

--- CountChart-React/src/App.js
+++ CountChart-React/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { FrameDisplay, RealTimeLineChart } from './Components';
 
 function App() {
   // Initial state with default data
@@ -8,10 +8,20 @@ function App() {
     { count: 0, groupCount: 0, time: '' } // Default data
   ]);
 
+  // State for frame data
+  const [frameData, setFrameData] = useState({ frame: '', time: '' });
+
+  const [groupThreshold, setGroupThreshold] = useState(35);
+
+  const handleGroupThresholdChange = (event) => {
+    const newValue = event.target.value;
+    setGroupThreshold(newValue);
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://192.168.18.132:8080/get_latest_processed_frame');
+        const response = await fetch(`http://192.168.100.10:8080/get_latest_processed_frame/${groupThreshold}`);
 
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -20,13 +30,16 @@ function App() {
         const jsonData = await response.json();
 
         // Extract count and groupCount from the response
-        const { count, groupCount, time } = jsonData;
+        const { count, groupCount, time, frame } = jsonData;
 
         // Create a new data object with count, groupCount, and time
         const newData = { count, groupCount, time };
 
         // Add the new data to the state
         setData((prevData) => [...prevData, newData]);
+
+        // Set the frame data in state
+        setFrameData({ frame, time });
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -36,27 +49,34 @@ function App() {
     fetchData();
 
     // Set up a timer to fetch data every 5 seconds (5000 milliseconds)
-    const interval = setInterval(fetchData, 1000);
+    const interval = setInterval(fetchData, 100);
 
     // Clean up the timer when the component unmounts
     return () => clearInterval(interval);
-  }, []);
+  }, [groupThreshold]);
 
   return (
     <div className="App">
       <h1>Real-Time Line Chart</h1>
-      <ResponsiveContainer width="70%" height={400}>
-        <LineChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 10 }}>
-          <XAxis dataKey="time" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="groupCount" name="Group Count" stroke="#82ca9d" />
-          <Line type="monotone" dataKey="count" name="Count" stroke="#8884d8" />
-        </LineChart>
-      </ResponsiveContainer>
+      <div className='container'>
+        <FrameDisplay frameData={frameData} />
+        <RealTimeLineChart data={data} />
+        <div className="threshold-control">
+          <label>Group Threshold:</label>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={groupThreshold}
+            onChange={handleGroupThresholdChange}
+          />
+          <span>{groupThreshold}</span>
+      </div>
     </div>
+  </div>
+  
   );
 }
 
 export default App;
+
