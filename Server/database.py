1cacaed67318430bee269149024e3b2c302d8b8c
HareemA <ahareem134@gmail.com>
2023-11-13 23:40:59 +0500

    Database file added

---
+++ Server/database.py
@@ -0,0 +1,44 @@
+import shutil
+import psycopg2
+import os
+import uuid
+
+conn = psycopg2.connect(
+    host="localhost",
+    port="5432",
+    dbname="SSS",
+    user="postgres",
+    password="12345"
+)
+
+def create_tables():
+    try:
+        with conn, conn.cursor() as cur:
+            cur.execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
+            
+            #Customer table
+            cur.execute("""create table if not exist parent(
+                id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                name VARCHAR NOT NULL,
+                gender VARCHAR NOT NULL,
+                age INT,
+                encoding VARCHAR,
+                created_at VARCHAR,
+                modified_at VARCHAR,
+                )""")
+            
+            #Visit table
+            cur.execute("""create table if not exist visits(
+                id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                customer_id UUID,
+                date VARCHAR,
+                time-in TIMESTAMP NOT NULL,
+                time_out TIMESTAMP,
+                group bool
+                FOREIGN KEY(customer_id) references customer(id)
+                )""")
+    except(Exception, psycopg2.DatabaseError) as error:
+        print("Couldnt create tables ",error)
+        
+
+        
