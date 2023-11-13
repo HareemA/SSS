1dc34839a946df9cc792364a92eaca4adad5a930
HareemA <ahareem134@gmail.com>
2023-11-13 23:46:19 +0500

    database file added

--- Server/database.py
+++ Server/database.py
@@ -17,28 +17,28 @@ def create_tables():
             cur.execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
             
             #Customer table
-            cur.execute("""create table if not exist parent(
+            cur.execute("""create table if not exists customer(
                 id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
                 name VARCHAR NOT NULL,
                 gender VARCHAR NOT NULL,
                 age INT,
                 encoding VARCHAR,
                 created_at VARCHAR,
-                modified_at VARCHAR,
+                modified_at VARCHAR
                 )""")
             
             #Visit table
-            cur.execute("""create table if not exist visits(
+            cur.execute("""create table if not exists visits(
                 id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
                 customer_id UUID,
                 date VARCHAR,
-                time-in TIMESTAMP NOT NULL,
+                time_in TIMESTAMP NOT NULL,
                 time_out TIMESTAMP,
-                group bool
+                group_val BOOLEAN,
                 FOREIGN KEY(customer_id) references customer(id)
                 )""")
     except(Exception, psycopg2.DatabaseError) as error:
         print("Couldnt create tables ",error)
         
 
-        
+create_tables()
